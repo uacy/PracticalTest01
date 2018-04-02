3a86a571169335577a43db00f51bc4e2504cebda
uacy <uacy_forever@yahoo.com>
2018-04-02 14:35:45 +0300

    ready for exam

--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest01/Constants.java
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01/Constants.java
@@ -2,7 +2,4 @@ package ro.pub.cs.systems.eim.practicaltest01;
 
 public class Constants {
 
-    public static int SERVICE_STOPPED = 0;
-    public static int NUMBER_OF_CLICKS_THRESHOLD = 1;
-    public static int SERVICE_STARTED = 1;
 }
